aa3d7c9f14d8c7daa76455461d9b5ded7e839eef
DivyanshSareen <sareendivyansh@gmail.com>
2022-11-27 12:13:23 +0530

    Initialize parking_lot endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ParkingLotController } from './parking_lot/parking_lot.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, ParkingLotController],
   providers: [AppService],
 })
 export class AppModule {}
